2d2f24779eb3d7e15b94b8d81d6161a4c749d030
Jayro Salgado <jsalgado@nearsoft.com>
2016-01-22 21:05:06 -0700

    payload

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,20 @@
 'use strict';
 
 exports.register = function (server, options, next) {
-    console.log('routes')
+    server.route({
+      method: 'GET',
+      path: '/gps',
+      handler: function (request, reply) {
+          console.log(request.payload)
+          reply('gps');
+      }});
+      server.route({
+      method: 'POST',
+      path: '/gps',
+      handler: function (request, reply) {
+          console.log(request.payload)
+          reply('gps');
+      }});
     next();
 };
 
