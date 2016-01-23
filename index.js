8a8f8516f4c3997206a7ff62f514250b0a6d800c
Jayro Salgado <jsalgado@nearsoft.com>
2016-01-22 19:25:33 -0700

    hapi again

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("listo----")
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: process.env.PORT });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start(() => {
+    console.log('Server running at:', server.info.uri);
+});
