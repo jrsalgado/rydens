b86a7fcecfd99a3790b9535809e8de62478ac3e6
Jayro Salgado <jsalgado@nearsoft.com>
2016-01-23 01:06:43 -0700

    s

--- routes/routes.js
+++ routes/routes.js
@@ -24,7 +24,7 @@ exports.register = function (server, options, next) {
       path: '/gps/{chofer}',
       handler: function (request, reply) {
           console.log(request.payload)
-          Point.find({chofer: request.params.chofer},{ limit:1, sort:{ date: -1 }})
+          Point.find({chofer: request.params.chofer})
           .where('loc')
           .exec(function (error, result) {
               reply({positions:result});
