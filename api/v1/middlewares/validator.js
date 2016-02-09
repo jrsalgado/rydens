cbec90ee112d77af012750afb4b68e115b48a7ec
Jayro Salgado <jsalgado@nearsoft.com>
2016-02-08 17:00:52 -0700

    get driver location

--- api/v1/middlewares/validator.js
+++ api/v1/middlewares/validator.js
@@ -6,11 +6,20 @@ function validator(expressJoi){
   return{
     users:{
       post:{
-        name: Joi.types.String().max(8),
+        name: Joi.types.String().max(30),
         lastName: Joi.types.String(),
         nsApiId: Joi.types.String(),
         age: Joi.types.Number(),
         driver: Joi.types.Boolean()
+      }
+    },
+    userLocation:{
+      patch:{
+        driver: Joi.types.String(),
+        name: Joi.types.String().max(30),
+        location:{
+          coordinates: Joi.types.Array(),
+        }
       }
     }
   }
