d7299d9c9007e840935a3d6ab964edf129f34ff8
Jayro Salgado <jsalgado@nearsoft.com>
2016-02-10 12:22:22 -0700

    use drivers routes on server

--- app/index.js
+++ app/index.js
@@ -2,13 +2,14 @@
 'use strict';
 
 module.exports= app;
-app.$inject = ['express', 'bodyParser', 'users.router'];
+app.$inject = ['express', 'bodyParser', 'users.router', 'drivers.router'];
 
-function app(express, bodyParser ,usersRouter){
+function app(express, bodyParser ,usersRouter, driversRouter){
   var v1 = express();
   
   v1.use(bodyParser.json());
   v1.use('/users', usersRouter);
+  v1.use('/drivers', driversRouter);
   v1.listen(process.env.PORT, function(){
     console.log('listen port: '+ process.env.PORT);
   });
