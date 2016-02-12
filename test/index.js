afdaaf5b2b63d3239b8c5a6b209638e688ef5584
Jayro Salgado <jsalgado@nearsoft.com>
2016-02-12 12:22:56 -0700

    set test enviroment

---
+++ test/index.js
@@ -0,0 +1,15 @@
+var di = require('ng-di');
+var server = require('../index.js');
+
+var test = di.module('test', ['server']);
+
+// Declare constants
+test.constant('httpMocks', require('express-mocks-http'));
+test.constant('chai-as-promised', require('chai-as-promised'));
+test.constant('chai', require('chai'));
+// Declare Factories
+test.factory('users', require('./users'));
+test.run(['users',function(users){
+
+}]);
+di.injector(['test']);
