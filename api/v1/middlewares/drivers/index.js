5cb8e0131bdb67145fe173f75e586a9b105f8e4f
Jayro Salgado <jsalgado@nearsoft.com>
2016-02-15 09:33:33 -0700

    middleware set as driver

--- api/v1/middlewares/drivers/index.js
+++ api/v1/middlewares/drivers/index.js
@@ -6,7 +6,8 @@ driversCtrls.$inject = ['model.user', 'q'];
 function driversCtrls(User, Q) {
 
   return {
-    fetchAllDrivers: fetchAllDrivers
+    fetchAllDrivers: fetchAllDrivers,
+    setAsDriver: setAsDriver
   }
   
   function fetchAllDrivers(req, res){
@@ -18,6 +19,30 @@ function driversCtrls(User, Q) {
         def.resolve(drivers);
       }
     });
+    return def.promise;
+  }
+  
+  function setAsDriver(req, res){
+    var query, promise, def = Q.defer();
+    
+    query = User.findOne({_id: req.params.id});
+    promise = query.exec();
+    query.then(success, error);
+    
+    function success(user){
+      if(!user){
+        def.reject({msg: "user not found"});
+      }else{
+        user.driver = true;
+        user.save();
+        def.resolve(user);
+      }
+    }
+
+    function error(err){
+      def.reject(err);
+    }
+
     return def.promise;
   }
   
--- test/drivers.js
+++ test/drivers.js
@@ -2,9 +2,9 @@
 /*global describe, it, before, after, beforeEach, afterEach*/
 
 module.exports = usersTest;
-usersTest.$inject = ['chai', 'chai-as-promised', 'q', 'mocks', 'httpMocks', 'drivers.controllers'];
+usersTest.$inject = ['chai', 'chai-as-promised', 'q', 'mocks', 'httpMocks', 'drivers.controllers', 'model.user'];
 
-function usersTest(chai, chaiAsPromised, q, mocks, httpMocks, middleware) {
+function usersTest(chai, chaiAsPromised, q, mocks, httpMocks, middleware, UserModel) {
   chai.use(chaiAsPromised);
   chai.should();
   
@@ -14,7 +14,7 @@ function usersTest(chai, chaiAsPromised, q, mocks, httpMocks, middleware) {
       it('should be an Array of drivers', function (done) {
         req = httpMocks.createExpressRequest();
         res = httpMocks.createExpressResponse();
-        middleware.fetchAllDrivers(req, res).should.eventually.be.an('array').notify(done)
+        middleware.fetchAllDrivers(req, res).should.eventually.be.an('array').notify(done);
       });
     });
     
@@ -46,12 +46,39 @@ function usersTest(chai, chaiAsPromised, q, mocks, httpMocks, middleware) {
       });
     });
     
-    describe.skip('PATCH /driver/set/:id', function () {
-      
+    describe('PATCH /driver/set/:id', function (done) {
+      var user1;
+      before(function(done){
+        UserModel.remove({}, function(err){
+        });
+        user1 = new UserModel(mocks.users.good);
+        user1.save(function(err, user){
+          done();
+        });
+      });
+
+      it('should not be rejected and set as driver', function (done) {
+        req = httpMocks.createExpressRequest({
+          params : { id: user1['_id'] }
+        });
+        res = httpMocks.createExpressResponse();
+        middleware.setAsDriver(req, res)
+        .should.be.fulfilled
+        .and.eventually.to.have.property("driver").equals(true)
+        .notify(done);
+      });
+
+      it('should be rejected if user not found', function (done){
+        req = httpMocks.createExpressRequest({
+          params:{ id: "344332234422331123456778" }
+        });
+        res = httpMocks.createExpressResponse();
+        middleware.setAsDriver(req, res)
+        .should.be.rejected.notify(done);
+      });
     });
     
     describe.skip('PATCH /driver/unset/:id', function () {
-      
     });
     
   });
